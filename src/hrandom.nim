a6099c38a5f8d878fbccbef4e27dc1abd08624b2
Azumabashi <>
2023-09-10 19:07:28 +0900

    [feat] define proc rand to get int value included to given range

--- src/hrandom.nim
+++ src/hrandom.nim
@@ -1,3 +1,5 @@
+import math
+
 type
   XorShift* = ref object
     x: int64
@@ -18,3 +20,9 @@ proc rand*(gen: XorShift, left, right: float64): (float64, XorShift) =
   let ratio = (float64(val) - float64(low(int64))) / (float64(high(int64)) - float64(low(int64)))
   let res = left + ratio * (right - left)
   (res, nextGen)
+
+proc rand*(gen: XorShift, left, right: int64): (int64, XorShift) =
+  assert left <= right
+  let offset = 0.499999999999
+  let (val, nextGen) = gen.rand(left.toFloat - offset, right.toFloat + offset)
+  (int64(round(val)), nextGen)
