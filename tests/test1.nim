1f3f6f5b6788c663e1976b958f602922fe40938c
Azumabashi <>
2023-09-10 19:27:22 +0900

    remove unused variable

--- tests/test1.nim
+++ tests/test1.nim
@@ -42,10 +42,7 @@ test "generate 2^{20} random values included to {-1, 0, 1}":
     check lb <= result and result <= ub
 
 test "shuffle seq":
-  let
-    lb = -1
-    ub = 1
-    before = @[0, 1, 2, 3, 4, 5]
+  let before = @[0, 1, 2, 3, 4, 5]
   var
     generator = init(42)
     after: seq[int] = @[]
