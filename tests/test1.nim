d776681a9bc3e6184f47660b7111d530c18fa305
Azumabashi <>
2023-09-10 19:25:23 +0900

    add test for shuffling seq

--- tests/test1.nim
+++ tests/test1.nim
@@ -40,3 +40,14 @@ test "generate 2^{20} random values included to {-1, 0, 1}":
   for _ in 0..<trials:
     (result, generator) = generator.rand(lb, ub)
     check lb <= result and result <= ub
+
+test "shuffle seq":
+  let
+    lb = -1
+    ub = 1
+    before = @[0, 1, 2, 3, 4, 5]
+  var
+    generator = init(42)
+    after: seq[int] = @[]
+  (after, generator) = before.shuffle(generator)
+  check after.toHashSet == before.toHashSet
