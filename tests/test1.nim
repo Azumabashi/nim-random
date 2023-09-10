5c4119e6bb0d756acc47b1eb155608a48d029803
Azumabashi <>
2023-09-10 18:55:10 +0900

    add test

--- tests/test1.nim
+++ tests/test1.nim
@@ -14,3 +14,17 @@ test "generate 2^{20} random values":
     results.incl(result)
   check results.len == trials
   
+test "generate 2^{20} random values included to [-1.5, 2.5]":
+  var
+    generator = init(42)
+    results = initHashSet[float64]()
+    result: float64 = 0.0
+    trials = 1 shl 20
+  let
+    lb = -1.5
+    ub = 2.5
+  for _ in 0..<trials:
+    (result, generator) = generator.rand(lb, ub)
+    check lb <= result and result <= ub
+    results.incl(result)
+  check results.len == trials
