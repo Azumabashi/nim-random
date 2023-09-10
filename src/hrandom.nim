75583cefde5e490a5b1e70b08aca040fb792b20c
Azumabashi <>
2023-09-10 15:57:38 +0900

    apply xor

--- src/hrandom.nim
+++ src/hrandom.nim
@@ -11,7 +11,7 @@ proc init*(seed: int64): XorShift =
 
 proc rand*(obj: XorShift): (int64, XorShift) =
   var nextObj = obj
-  nextObj.x = nextObj.x shl 13
-  nextObj.x = nextObj.x shr 7
-  nextObj.x = nextObj.x shl 17
+  nextObj.x = nextObj.x xor (nextObj.x shl 13)
+  nextObj.x = nextObj.x xor (nextObj.x shr 7)
+  nextObj.x = nextObj.x xor (nextObj.x shl 17)
   (nextObj.x, nextObj)
